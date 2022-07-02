5c94ee7b28e2e45726831c67be3c63229fe5c0e2
DiegoFerraz07 <dihferraz-01@hotmail.com>
2022-07-02 15:25:41 -0300

    homescreen

--- App.js
+++ App.js
@@ -2,12 +2,12 @@ import React from 'react';
 import {Text, View, SafeAreaView} from 'react-native';
 import {NavigationContainer} from '@react-navigation/native';
 
-import Router from './src/router';
+import HomeScreen from './src/containers/index';
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Router />
+      <HomeScreen />
     </NavigationContainer>
   );
 }
---
+++ src/containers/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import {createMaterialTopTabNavigator} from '@react-navigation/material-top-tabs';
+
+import Home from '../containers/home';
+import Status from '../containers/status';
+import Chamadas from '../containers/chamadas';
+
+const Tab = createMaterialTopTabNavigator();
+
+export default function HomeScreen() {
+  return (
+    <Tab.Navigator>
+      <Tab.Screen name="Conversas" component={Home} />
+      <Tab.Screen name="Status" component={Status} />
+      <Tab.Screen name="Chamadas" component={Chamadas} />
+    </Tab.Navigator>
+  );
+}
