59242a77917a944a7ca3a19e17ad03e844f8a65d
DiegoFerraz07 <dihferraz-01@hotmail.com>
2022-07-02 14:58:44 -0300

    atualizacao

--- App.js
+++ App.js
@@ -1,10 +1,13 @@
-import {View, Text} from 'react-native';
 import React from 'react';
+import {Text, View, SafeAreaView} from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
+
+import Router from './src/router';
 
 export default function App() {
   return (
-    <View>
-      <Text>App</Text>
-    </View>
+    <NavigationContainer>
+      <Router />
+    </NavigationContainer>
   );
 }
---
+++ src/containers/chamadas/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import {Text, View} from 'react-native';
+
+export default function Chamadas() {
+  return (
+    <View>
+      <Text>Chamadas</Text>
+    </View>
+  );
+}
